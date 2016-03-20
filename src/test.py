4f20039537181f4b6bf572fced69231d91a6ab5f
Sahithi Chaganti <sahithi@localhost.localdomain>
2016-03-19 23:23:44 -0600

    working sift features

--- src/test.py
+++ src/test.py
@@ -1,5 +1,9 @@
 import cv2
 import numpy as np
+
+import sys
+
+sys.path.append('/home/sahithi/opt/opencv/release/lib/')
  
 img = cv2.imread('home.jpg')
 gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -7,6 +11,6 @@ gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 sift = cv2.xfeatures2d.SIFT_create()
 kp = sift.detect(gray,None)
  
-img=cv2.drawKeypoints(gray,kp)
+cv2.drawKeypoints(gray,kp,img)
 
 cv2.imwrite('sift_keypoints.jpg',img)
