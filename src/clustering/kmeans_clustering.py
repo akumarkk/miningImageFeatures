698e4d7af0949926dc831c2782af4a5de9ce731d
Anil Kumar Konasale Krishna <akumarkk@cs.utah.edu>
2016-03-20 19:00:10 -0600

    K Means Clustering

--- src/clustering/kmeans_clustering.py
+++ src/clustering/kmeans_clustering.py
@@ -15,11 +15,36 @@ def print_image(image):
 
 def imageHistograms():
     images = []
-    for file in scandir.scandir('../dataSets/'):
+    for file in scandir.scandir('../../dataSets/'):
         if file.is_file():
             images.append(file.path)
 
-    print images
+    # convert each image into Grey image
+    greyImages = []
+    imgArray = []
+    for img_file in images:
+        img = cv2.imread(img_file)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        imgHist = cv2.calcHist([img],[0],None,[256],[0,256])
+        cv2.normalize(imgHist, imgHist, 0, 255, cv2.NORM_MINMAX, -1)
+        
+        #hist=np.int32(np.around(hist_item))
+
+        imgHist = imgHist.transpose()
+
+        imgArray.append(imgHist)
+
+    return imgArray
+
+def kmeanClustering(inputImages):
+    k = 20
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret, label, center = cv2.kmeans(inputImages, k, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
+
+    
+
+
 
 
 img = cv2.imread('home.jpg')
